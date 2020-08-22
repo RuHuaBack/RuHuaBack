c974251e045d3e1554dc32e9db381eb96048c21f
思廷 陳 <teresa50530@gmail.com>
2020-08-23 01:58:15 +0800

    first

---
+++ RunHuaAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RunHuaAdmin.MvcApplication" Language="C#" %>
---
+++ RunHuaAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RunHuaAdmin.Startup))]
+namespace RunHuaAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
