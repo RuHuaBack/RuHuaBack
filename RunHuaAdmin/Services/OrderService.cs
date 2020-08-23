7af23fff9b6ce6290336fe82fa60c8b4feea9125
Bla3ehy <aa4152@gmail.com>
2020-08-23 22:24:19 +0800

    add repository

---
+++ RunHuaAdmin/Services/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RunHuaAdmin.Models;
+using RunHuaAdmin.Repository;
+
+namespace RunHuaAdmin.Services
+{
+    public class OrderService
+    {
+        OrderRepository _repo = new OrderRepository();
+        public List<OrderViewModel> GetOrderService()
+        {
+            var tempList = _repo.GetAllOrder();
+            List<OrderViewModel> newResult = new List<OrderViewModel>();
+            int o = 0;
+            foreach (var i in tempList)
+            {
+                newResult.Add(new OrderViewModel(){CompanyName = i.CompanyName, StudentName = tempList[o].StudentName, Color = i.Color});
+            }
+
+            return null;
+        }
+    }
+}
